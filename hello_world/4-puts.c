d7fa2572593bad85d9b7b1b261cbbe8f2ed510f2
root <jehp01104006@gmail.com>
2022-06-13 13:21:39 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly text following a new line
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

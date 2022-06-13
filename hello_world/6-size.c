d373f3f88d55d4a23944e7064127359050610fee
root <jehp01104006@gmail.com>
2022-06-13 14:59:08 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ *  * main - prints a message
+ *   * Return: Always 0
+ *    */
+
+int main(void)
+{
+	printf("Size of a char: byte  %zu", sizeof(char));
+	printf("Size of an int: byte  %zu", sizeof(int));
+	printf("Size of an long int: byte  %zu", sizeof(long int));
+	printf("Size of an long long int: byte  %zu", sizeof(long long int));
+	printf("Size of an float: byte  %zu", sizeof(float));
+	return (0);
+}

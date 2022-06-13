3e856d011d0af4ad0cd6aca41aff1352625a69ad
root <jehp01104006@gmail.com>
2022-06-13 15:05:59 -0700

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,14 +1,17 @@
+#include <stdio.h>
+
 /**
- *  * main - prints a message
- *   * Return: Always 0
- *    */
+ * main - print the size of a date
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	printf("Size of a char: byte  %zu", sizeof(char));
-	printf("Size of an int: byte  %zu", sizeof(int));
-	printf("Size of an long int: byte  %zu", sizeof(long int));
-	printf("Size of an long long int: byte  %zu", sizeof(long long int));
-	printf("Size of an float: byte  %zu", sizeof(float));
+	printf("Size of a char: %zu", sizeof(char));
+	printf("Size of an int: %zu", sizeof(int));
+	printf("Size of an long int: %zu", sizeof(long int));
+	printf("Size of an long long int: %zu", sizeof(long long int));
+	printf("Size of an float: %zu", sizeof(float));
 	return (0);
 }

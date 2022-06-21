75312cc395baff98f946ec4846478aff1386c51e
Jehp00 <Jehp01104006@gmail.com>
2022-06-21 14:03:43 -0500

    putchar more numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all numbers except 2 and 4
+ * Return: 0 on success.
+ */
+
+void print_most_numbers(void)
+{
+int c = 0;
+
+while (c <= 9)
+{
+if (c != 2 && c != 4)
+{
+_putchar (c + '0');
+++c;
+}
+}
+_putchar('\n');
+}

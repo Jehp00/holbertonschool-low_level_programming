1ef8454197431da545a664aa2641b38f4586af30
Jehp00 <Jehp01104006@gmail.com>
2022-06-28 14:35:53 -0500

    string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+char *string_toupper(char *str)
+{
+    int a;
+
+    for (a = 0; str[a] != '\0'; a++)
+    {
+        if (str[a] >= 'a' && str[a] <= 'z')
+        {
+            str[a] = str[a] - 32;
+        }
+    }
+    return (str);
+}

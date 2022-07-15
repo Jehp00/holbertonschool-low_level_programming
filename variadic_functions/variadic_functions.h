38f172f89e402fdcb1416e050fb56fd3f5017dc8
Jehp00 <Jehp01104006@gmail.com>
2022-07-15 17:01:55 -0500

    solve of 3 with the structure

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,23 +2,24 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-
-typedef struct print
-{
-char *t;
-void (*f)(va_list);
-} print_t;
+#include <stdio.h>
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct print - print type with corresponding print function
+ * @st: print type
+ * @p: pointer
+ */
+
+typedef struct st
+{
+	char *st;
+	void (*p)();
+} print_t; 
 
 #endif /*VAIADIC_FUNCTIONS_H*/

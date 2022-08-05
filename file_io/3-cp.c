617e894c7c8d4593967f6b997d13f8b29d7c0200
Jehp00 <Jehp01104006@gmail.com>
2022-08-05 13:45:41 -0500

    copy the content form another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,9 +3,91 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * copy_cont - copies the content of a file to another file
+ * checker_97 - checks for the corret number of the arguments
+ * @argc: number of arguments
+ *
+ * Return: void
+ */
+
+void checker_97(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * checker_98 - checks the file_from exists and can be read
+ * @check: checks if is true or false
+ * @file: file_from's name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+
+void checker_98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * checker_99 - checks that file_to was create and can be written
+ * @check: chescks if id true or false
+ * @file: file_to's name
+ * @fd_from: file descriptor or -1
+ * @fd_to: file descriptor or -1
+ *
+ * Return: void
+ */
+
+void checker_99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
+	}
+}
+
+/**
+ * checker_100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+
+void checker_100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+		
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
  * @argv: array of arguments
  *
@@ -14,44 +96,30 @@
 
 int main(int argc, char *argv[])
 {
-	int f_read, f_write, r, i, j;
-	char buff[BUFSIZ];
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t l_read, l_write;
+	char buff[1024];
+	mode_t file_perm;
 
-	if (argc != 3)
+	checker_97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	checker_98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	checker_99((ssize_t)fd_to, argv[2], fd_from, -1);
+	l_read = 1024;
+	while (l_read == 1024)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit(97);
+		l_read = read(fd_from, buff, 1024);
+		checker_98(l_read, argv[1], fd_from, fd_to);
+		l_write = write(fd_to, buff, l_read);
+		if (l_write != l_read)
+			l_write = -1;
+		checker_99(l_write, argv[2], fd_from, fd_to);
 	}
-	f_read = open(argv[1], O_RDONLY);
-	if (f_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	f_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(f_write, buff, BUFSIZ)) > 0)
-	{
-		if (f_write < 0 || write(f_write, buff, r) != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(f_read);
-			exit(99);	
-		}
-	}
-	if (r < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	i = close(f_read);
-	j = close(f_write);
-	if (i < 0 || j < 0)
-	{
-		if (i < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_read);
-		if (j < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_write);
-		exit(100);
-	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	checker_100(close_to, fd_to);
+	checker_100(close_from, fd_from);
 	return (0);
 }
